daf3d80b55606f89b96d08f573b1a138b52d525f
charlez1998 <charlesjacobtan1@gmail.com>
2021-08-06 21:19:37 +0800

    add page functions

--- Classifying X-ray Images for COVID-19/covxray_webapp.py
+++ Classifying X-ray Images for COVID-19/covxray_webapp.py
@@ -10,11 +10,32 @@ from skimage.transform import resize
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
+
 print("Loading model")
+
 global sess
 sess = tf.Session()
 set_session(sess)
 global model
-model = load_model('my_cifar10_model.h5')
+model = load_model('covid_model.h5')
 global graph
 graph = tf.get_default_graph()
+
+@app.route('/', methods = ['GET', 'POST'])
+def main_page():
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join('uploads', filename))
+        return redirect(url_for('prediction', filename = filename))
+    return render_template('index.html')
+
+@app.route('/prediction/<filename>')
+def prediction(filename):
+    my_image = plt.imread(os.path.join('uploads', filename))
+    my_image_re = resize(my_image, (32,32,3))
+
+    with graph.as_default():
+        set_session(sess)
+        probabilities = model.predict(np.array([my_image_re,]))[0,:]
+        print(probabilities)
