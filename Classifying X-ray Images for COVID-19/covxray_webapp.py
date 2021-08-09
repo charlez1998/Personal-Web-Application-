3cf4508dada12c45fcaa50d60854e8f93c8ee171
charlez1998 <charlesjacobtan1@gmail.com>
2021-08-09 23:48:33 +0800

    edit webapp to load graph execution version

--- Classifying X-ray Images for COVID-19/covxray_webapp.py
+++ Classifying X-ray Images for COVID-19/covxray_webapp.py
@@ -14,12 +14,10 @@ import numpy as np
 print("Loading model")
 
 global sess
-sess = tf.Session()
+sess = tf.compat.v1.Session()
 set_session(sess)
-global model
-model = load_model('covid_model.h5')
 global graph
-graph = tf.get_default_graph()
+graph = tf.compat.v1.get_default_graph()
 
 @app.route('/', methods = ['GET', 'POST'])
 def main_page():
@@ -36,6 +34,7 @@ def prediction(filename):
     my_image_re = resize(my_image, (32,32,3))
 
     with graph.as_default():
+        model = load_model('covid_model.h5')
         set_session(sess)
         probabilities = model.predict(np.array([my_image_re,]))[0,:]
         print(probabilities)
